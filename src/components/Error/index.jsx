f312a92432435b6453c7896b96afe663bb30a8d0
Alexandre Peyrolles <apeyrolles34@gmail.com>
2025-11-05 10:36:20 +0100

    feat: Ajout du lien de retour à l'accueil depuis la page erreur

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -1,7 +1,11 @@
+import { Link } from 'react-router-dom'
+
 function Error() {
     return (
         <div>
-            <h1>Oups 🙈 Cette page n'existe pas</h1>
+            <span>404</span>
+            <h1>Oups! La page que vous demandez n'existe pas.</h1>
+            <Link to='/'>Retourner sur la page d'accueil</Link>
         </div>
     )
 }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,7 +5,7 @@ function Header() {
     return (
         <nav>
             <img src={headerLogo} alt="Logo" />
-            <Link to="/">Accueil</Link>
+            <Link to='/'>Accueil</Link>
             <Link to='/About'>A propos</Link>
         </nav>
     )
