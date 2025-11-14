240ee74b56fa15cc3672014a028bad26d614aaad
Alexandre Peyrolles <apeyrolles34@gmail.com>
2025-11-14 10:00:13 +0100

    fix: Ajout d'un composant Router pour répondre à l'énoncé du projet

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 
 import Home from './pages/Home/Home.jsx'
 import About from './pages/About/About.jsx'
-{/*import Logements from './pages/Logements/index.jsx'*/}
+import Logements from './pages/Logements/Logements.jsx'
 import Error from './components/Error/Error.jsx'
 
 const AppRouter = () => {
@@ -12,7 +12,8 @@ const AppRouter = () => {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
-      {/*<Route path="/logements/:id" element={<Logements />} />*/}
+      {/*<Route path="/logements" element={<Logements />} />*/}
+      <Route path="/logements/:id" element={<Logements />} />
       <Route path="*" element={<Error />} />
     </Routes>
 
--- src/main.jsx
+++ src/main.jsx
@@ -21,21 +21,3 @@ const App = () => {
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
 
 export default App;
-
-{/*const container = document.getElementById('root')
-const root = createRoot(container)
-root.render(
-  <React.StrictMode>
-    <Router>
-      <Header />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/logements' element={<Logements />} />
-        {/* <Route path='/logements/{id}' element={<LogementDetail />} />
-        <Route path='/about' element={<About />} />
-        <Route path='*' element={<Error />} />
-      </Routes>
-      <Footer />
-    </Router>
-  </React.StrictMode>
-)*/}
