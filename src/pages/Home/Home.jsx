054fbbab301971aa551b87f6dbe7d515d1f3c6b0
Alexandre Peyrolles <apeyrolles34@gmail.com>
2025-11-17 09:38:35 +0100

    feat: Intégration de la page Logement avec fonction pour afficher la page en fonction de l'id sur les carte de la page Home et intégration du composant Slideshow

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,8 @@ import falaise from '../../assets/falaise.png';
 import Banner from '../../components/Banner/Banner';
 import Card from '../../components/Card/Card';
 import '../../styles/index.scss';
-
+import { Link } from 'react-router-dom';
+import logements from '../../../public/logements.json';
 
   // hooks
   //useState
@@ -10,13 +11,26 @@ import '../../styles/index.scss';
   // logique de code
 
 function Home() {
+
   // rendu
   return (
     <div className="container">
-      <Banner image={falaise} altText="Des falaises en bord de mer" 
-      title="Chez vous, partout et ailleurs" />
+      <Banner 
+        image={falaise} 
+        altText="Des falaises en bord de mer" 
+        title="Chez vous, partout et ailleurs" 
+      />
       <section className="cards__container">
-        <Card />
+        <ul className="cards__list">
+          {logements.map((logement) => (
+            <Link key={logement.id} to={`/logements/${logement.id}`}>
+              <Card 
+                title={logement.title}
+                cover={logement.cover}
+              />
+            </Link>
+          ))}
+        </ul>
       </section>
     </div>
   )
--- src/pages/Logements/Logements.jsx
+++ src/pages/Logements/Logements.jsx
@@ -1,24 +1,22 @@
+import { useParams } from 'react-router-dom';
 import logements from "../../../public/logements.json";
 import Slideshow from '../../components/Slideshow/Slideshow';
 import Dropdown from '../../components/Dropdown/Dropdown';
-import '../../styles/index.scss';
 
 
 function Logements() {
-  const logementInfos = logements.map((logement) =>
-    <div key={logement.id}>
+  const { id } = useParams();
+  const logement = logements.find(l => l.id === id);
+
+
+  return (
+    <div className='container'>
 
       <Slideshow 
         pictures={logement.pictures}
         title={logement.title}
       />
 
-    </div>
-  );
-
-  return (
-    <div className="container">
-      {logementInfos}
     </div>
   );
 }
